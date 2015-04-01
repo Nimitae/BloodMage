b9bd2bb5665d12e3c2868f91d9d81449ebae2168
Nimitae <nimitae@hotmail.com>
2015-04-02 03:44:10 +0800

    Implemented heavy melee tree

---
+++ Assets/ShockwaveProjectile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockwaveProjectile : MonoBehaviour {
+	[HideInInspector]
+	public float projectileDamage;
+	[HideInInspector]
+	public float projectileSpeed;
+	[HideInInspector]
+	public float projectileForce;
+	[HideInInspector]
+	public float projectileDuration;
+
+	private float timeProjectileExpire;
+	private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		timeProjectileExpire = Time.time + projectileDuration;
+		rigid = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveVector = transform.right * projectileSpeed;
+		//	rigid.AddForce(new Vector2(moveVector.x , moveVector.y));
+		rigid.velocity = new Vector2 (moveVector.x, moveVector.y);
+
+		if (Time.time > timeProjectileExpire) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.transform.tag == "Enemy") {
+			col.gameObject.GetComponent<IEnemy> ().takeDamage (projectileDamage);
+			Destroy (gameObject);
+		} else if (col.transform.tag == "Ground") {
+			Destroy (gameObject);
+		} else if (col.transform.tag == "Boundary") {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/SpellOneShockwave.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellOneShockwave : MonoBehaviour {
+	public Transform shockwave;
+	public float shockwaveDamage;
+	public float shockwaveSpeed;
+	public float shockwaveDuration;
+	
+	void Start()
+	{
+		float xPosition = transform.position.x;
+		Quaternion rotationQuart;
+		if (transform.rotation.z == 1){
+			rotationQuart = new Quaternion(0,0,1,0);
+			xPosition -= 0.2f;
+		} else {
+			rotationQuart = new Quaternion(0,0,0,0);
+			xPosition += 0.2f;
+		}
+		Vector3 shockwavePos = new Vector3(xPosition, transform.position.y,0);
+		Transform newProjectile =(Transform) Instantiate(shockwave,shockwavePos,Quaternion.identity * rotationQuart);
+		ShockwaveProjectile script = newProjectile.GetComponent<ShockwaveProjectile>(); 
+		script.projectileDamage = shockwaveDamage;
+		script.projectileSpeed = shockwaveSpeed;
+		script.projectileDuration = shockwaveDuration;
+	}
+}
